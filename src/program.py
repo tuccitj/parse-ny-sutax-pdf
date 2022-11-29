5436602e67f6d51257305bb6d4a9823e1927a039
tuccitj <71199179+tuccitj@users.noreply.github.com>
2022-11-29 08:06:28 -0500

    cleaned class

---
+++ src/program.py
@@ -0,0 +1,8 @@
+from nyratepdf2csv import NYPub718Parser
+parser = NYPub718Parser('docs/')
+# tabula extracts tables from PDFs
+# df = tabula.read_pdf('docs/core/pub718.pdf', multiple_tables=True, pages='all')
+parser = NYPub718Parser('docs/core/pub718.pdf')
+parser.result_aggregate.to_csv("docs/output/ny_rates_aggregate.csv", index=False)
+parser.result_county_rates.to_csv("docs/output/ny_county_rates.csv", index=False)
+parser.result_city_rates.to_csv("docs/output/ny_city_rates.csv", index=False)
